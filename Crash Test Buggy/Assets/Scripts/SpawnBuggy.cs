e5b89836f321f8fa44db0b957a6266667276afdb
Aleksandar Andrejic <andrejic.aleks91@gmail.com>
2024-06-06 18:09:18 +0200

    Fix Menu switch with buggy respawn

--- Crash Test Buggy/Assets/Scripts/SpawnBuggy.cs
+++ Crash Test Buggy/Assets/Scripts/SpawnBuggy.cs
@@ -9,6 +9,7 @@ public class SpawnBuggy : MonoBehaviour
 {
     public GameObject buggyCrashTest;
     public GameObject buggyCrashTestPrefab;
+    public GameObject mainCamera;
 
     // private bool _oldBuggyDestroyed;
     private bool _waitForNewSpawn;
@@ -30,10 +31,12 @@ public class SpawnBuggy : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //Instantiate new buggy and remove old one
             GameObject newBuggy = Instantiate(buggyCrashTestPrefab, new Vector3(150, 0.5f, 100), Quaternion.identity);
             _oldCarBehaviour.StopFmodEngineSound();
             Destroy(buggyCrashTest);
 
+            //Setup new buggy
             buggyCrashTest = newBuggy;
             MeshDeformer meshDeformerScript = buggyCrashTest.GetComponent<MeshDeformer>();
             CarBehaviour newCarBehaviourScript = buggyCrashTest.GetComponent<CarBehaviour>();
@@ -43,11 +46,11 @@ public class SpawnBuggy : MonoBehaviour
             newCarBehaviourScript.speedText = _oldCarBehaviour.speedText;
             buggyCrashTest.name = "Buggy";
             
-            
-            
-            GameObject mainCamera = GameObject.Find("Main Camera");
+            //Setup camera values
             SmoothFollow followScript = mainCamera.GetComponent<SmoothFollow>();
             followScript.target = buggyCrashTest.transform;
+            MenuScene1Behaviour menuBehaviour = mainCamera.GetComponent<MenuScene1Behaviour>();
+            menuBehaviour.buggy = buggyCrashTest;
         }
     }
 
